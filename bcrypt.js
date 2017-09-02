f3b6ad17f4eebd38205d2a4044916d33858f85c2
SeongJin Ahn <sjahn@astams.com>
2017-09-02 11:22:36 +0900

    change for pkg module

--- bcrypt.js
+++ bcrypt.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var binary = require('node-pre-gyp');
-var path = require('path');
-var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
-var bindings = require(binding_path);
+// var binary = require('node-pre-gyp');
+// var path = require('path');
+// var binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+// var bindings = require(binding_path);
+var bindings = require("bindings")("bcrypt_lib");
 
 var crypto = require('crypto');
 
